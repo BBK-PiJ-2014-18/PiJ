e2dc1b194ab454e03eeb2df77c0beb29f0cc18cf
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2014-10-28 17:40:00 +0000

    put methods inside a chessboard class, can add, remove and count queens now

--- day5/eightDames.groovy
+++ day5/eightDames.groovy
@@ -1,26 +1,46 @@
-boolean[][] placeEight (int x, int y) {
-	boolean[][] result;
-	result = new boolean[8][8];
-	result[x][y] = true;
-	return result;
-}
+class ChessBoard {
+	boolean[][] queenBoard = new boolean[8][8];
+	int totalQueens = 0;
 
-void printBoard(boolean[][] chessBoard) {
-	int i, j;
-	for (i = 0; i <=7; i++) {
-		for (j = 0; j <= 7; j++) {
-			if(chessBoard[i][j]) {
-				System.out.print("X");
-			} else {
-				System.out.print(".");
-			}		
+	boolean[][] placeQueen (int x, int y) {
+		queenBoard[x][y] = true;
+		totalQueens++;
+		return queenBoard;
+	}
+
+	boolean[][] removeQueen (int x, int y) {
+		queenBoard[x][y] = false;
+		totalQueens --;
+		return queenBoard;		
+	}
+
+	void printBoard() {
+		System.out.println("trying to print");
+		int i, j;
+		for (i = 0; i <=7; i++) {
+			for (j = 0; j <= 7; j++) {
+				if(queenBoard[i][j]) {
+					System.out.print("X ");
+				} else {
+					System.out.print("- ");
+				}		
+			}
+			System.out.println();
 		}
 		System.out.println();
+		System.out.println("Total queens is: " + totalQueens);
+		System.out.println();
+		
 	}	
-}	
+
+
+}
 	
-	
-boolean[][] chessBoard;
-chessBoard = new boolean[8][8];
-chessBoard = placeEight(3,3);
-printBoard(chessBoard);
+test = new ChessBoard();
+test.printBoard();
+test.placeQueen(3,3);
+test.placeQueen(4,4);
+test.placeQueen(5,5);
+test.printBoard();
+test.removeQueen(4,4);
+test.printBoard();
