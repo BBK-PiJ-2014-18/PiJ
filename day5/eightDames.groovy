ae9c176e7105350a23e960fdebb6ca57db4fff02
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2014-10-28 20:18:53 +0000

    added validPlacement method and the recursion.  Works.

--- day5/eightDames.groovy
+++ day5/eightDames.groovy
@@ -1,4 +1,8 @@
+// bit confused about why used "this." in some places and not others, 
+// but works in groovy anyway!  (And...Recursion part not really to do with that aspect).
+
 class ChessBoard {
+
 	boolean[][] queenBoard = new boolean[8][8];
 	int countQueens = 0;
 
@@ -15,7 +19,7 @@ class ChessBoard {
 	}
 
 	void printBoard() {
-		System.out.println("trying to print");
+		System.out.println();
 		int i, j;
 		for (i = 0; i <=7; i++) {
 			for (j = 0; j <= 7; j++) {
@@ -32,50 +36,71 @@ class ChessBoard {
 		System.out.println();
 	}	
 
-	boolean vaildPlacement(int x, int y) {
+	boolean validPlacement(int x, int y) {
+		// check row and column	
+		for (int j = 0; j <= 7; j++) {
+			if(this.queenBoard[x][j] == true) {	
+				return false;
+			}
+			if(this.queenBoard[j][y] == true) {
+				return false;
+			}
+		}
+		// check diagonals
+		for (int j = 0; j <= 7; j++) {
+			if (x-j >= 0 && y-j >= 0) {
+				if(this.queenBoard[x-j][y-j] == true) {
+					return false;
+				}	
+			}	
+			if (x+j <= 7 && y-j >= 0) {
+				if(this.queenBoard[x+j][y-j] == true) {
+					return false;
+				}	
+			}	
+			if (x-j >= 0 && y+j <= 7) {
+				if(this.queenBoard[x-j][y+j] == true) {
+					return false;
+				}	
+			}	
+			if (x+j <= 7 && y+j <= 7) {
+				if(this.queenBoard[x+j][y+j] == true) {
+					return false;
+				}	
+			}	
+		}
 		return true;
 	}
 
-	boolean play(int countQueens) {
+
+	boolean playQueens(int countQueens) {
 		
 		if(countQueens == 8) {
 			return true;
-		}
+		} else {
 		
-		for (i=0; i <=7; i++) {
-		
-			for (j=0; j<=7; j++) {
+			for (int i=0; i <=7; i++) {
+				for (int j=0; j<=7; j++) {
 				
-				if(validPlacement(i,j)) {
-					placeQueen(i,j);			
-				
-					if(play(countQueens)) {
-						return true;
-					} else {
-						removeQueen(i,j);
-					}				
-				}		
-			}
+					if(validPlacement(i,j)) {
+							placeQueen(i,j);			
+						if(playQueens(this.countQueens)) {
+							return true;
+						} else {
+							removeQueen(i,j);
+						}				
+					}		
+
+				}
+			}			
 		}
-		
+		return false;
 	}	
-		
-		
-		
 }
+
 test = new ChessBoard();
+test.playQueens(1);
 test.printBoard();
 
 
 
-
-/* testing placing, removing, counting and printing board	
-test = new ChessBoard();
-test.printBoard();
-test.placeQueen(3,3);
-test.placeQueen(4,4);
-test.placeQueen(5,5);
-test.printBoard();
-test.removeQueen(4,4);
-test.printBoard();
-*/
