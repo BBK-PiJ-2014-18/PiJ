6fbea6d22f3d83f7de02471cab2acfe553f3c765
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2014-11-12 12:49:30 +0000

    finished

--- day7/circularLists/Patient.java
+++ day7/circularLists/Patient.java
@@ -16,25 +16,19 @@ public class Patient {
 	public String getName() {
 		return name;
 	}
-	
 	public int getAge() {
 		return age;
 	}
-	
 	public String getIllness() {
 		return illness;
 	}			
-
 	public Patient getNextPatient() {
 		return nextPatient;
 	}
-
 	//setter
 	public void setNextPatient(Patient nextPatient) {
 		this.nextPatient = nextPatient;
 	}
-
-
 
 	//add a patient to list
 	public void addPatient(Patient newPatient) {
@@ -50,13 +44,14 @@ public class Patient {
 			}
 		}
 	}
-	// new delete patient - need to fix for not found
+
+	// delete patient
 	public boolean deletePatient(Patient patient) {
 		boolean done = false;
 		Patient currentPatient = this;
 		
 		while (!done) {
-			if(currentPatient.nextPatient == patient) {
+			if(currentPatient.nextPatient.name.equals(patient.name)) {
 				// the next one is the one to delete - DO STUFF HERE
 				currentPatient.nextPatient = patient.nextPatient;
 				done = true;
@@ -65,28 +60,13 @@ public class Patient {
 			
 				currentPatient = currentPatient.nextPatient;
 				if (currentPatient.nextPatient == this) {
+					//been around once
 					return false;
 				}	
 				currentPatient = currentPatient.nextPatient;
 			}	
-		}		
+		}	
+		//there was a problem if here so...	
 		return false;
 	}	
-}				
-/*	//OLD delete a patient from list
-	public boolean deletePatient(Patient patient) {
-	
-		if(this.nextPatient == null) {
-			//patient to be removed was not found
-			return false;
-		} else if (this.nextPatient.name.equals(patient.name)) {
-			//we found it...it is the next one...	
-			//now link this patient to the one after the next
-			this.nextPatient = nextPatient.nextPatient;
-			return true;
-		} else {
-			return this.nextPatient.deletePatient(patient);	
-		}	
-			
-	}
-*/	
+}	
