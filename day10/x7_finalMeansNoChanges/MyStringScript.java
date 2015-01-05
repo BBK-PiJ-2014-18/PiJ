57491f286780d11fc18d7c55bdd9814360357423
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-01-05 14:08:49 +0000

    done

---
+++ day10/x7_finalMeansNoChanges/MyStringScript.java
@@ -0,0 +1,16 @@
+
+public class MyStringScript {
+
+	public static void main(String[] args) {
+		MyStringScript mss = new MyStringScript();
+		mss.launch();
+	}
+
+	public void launch() {
+	
+		MyString ms = new MyString("markkingsbury");
+		ms.printEven();
+	}
+
+
+}
