7328892509ac231609d91b3bb273f7b01f0b8f85
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-01-06 14:42:10 +0000

    Same, just deleted (confused?) notes

--- day11/x1_dontRepeatYourself/ComparatorScript.java
+++ day11/x1_dontRepeatYourself/ComparatorScript.java
@@ -12,18 +12,5 @@ public class ComparatorScript {
 		System.out.println(comp.returnMax("50","60"));
 		System.out.println(comp.returnMax(1.2,1.3));
 		
-		
-		/*THIS ALSO WORKS
-		Comparator<String> compS = new Comparator<String>();
-		System.out.println(compS.returnMax("100", "200"));
-
-		Comparator<Double> compD = new Comparator<Double>();
-		System.out.println(compD.returnMax(100.5, 200.5));
-
-		Comparator<Integer> compI = new Comparator<Integer>();
-		System.out.println(compI.returnMax(1, 2));	
-		*/
-	}
-
-
+	}		
 }
