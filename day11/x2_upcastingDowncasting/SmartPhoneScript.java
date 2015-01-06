286149b0596ee92c1a9de0c7b4b0dc4992f32e5a
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-01-06 17:03:16 +0000

    2.2 Direct Upcasting, done

--- day11/x2_upcastingDowncasting/SmartPhoneScript.java
+++ day11/x2_upcastingDowncasting/SmartPhoneScript.java
@@ -5,8 +5,11 @@ public class SmartPhoneScript {
 	System.out.println();
 	System.out.println("=== 2.1 Start ===");
 	sps.launch1();
+	System.out.println("=== 2.2 Start ===");
+	sps.launch2();
 	}
 
+//	2.1 Set-up works fine
 	public void launch1() {
 		System.out.println();
 		SmartPhone myPhone = new SmartPhone("nokia");
@@ -20,9 +23,38 @@ public class SmartPhoneScript {
 		myPhone.ringAlarm("wk 11 alarm");
 		System.out.println();
 	}
+
+//	2.2.1 Won't compile these three methods in SmartPhone....
+	public void launch2() {
+		System.out.println();
+		MobilePhone myPhone = new SmartPhone("nokia");
+		System.out.println("Brand: " + myPhone.getBrand());
+//		myPhone.browseWeb("Page to browse");
+//		System.out.println(myPhone.findPosition());
+		myPhone.call("1234");
+		myPhone.printLastNumbers();
+		myPhone.call("001234");
+//		myPhone.playGame("wk 11 game");
+		myPhone.ringAlarm("wk 11 alarm");
+		System.out.println();
+	
+//	2.2.2 Can fix by DIRECT UPCASTING to a SmartPhone....
+
+		System.out.println("===> ====> NOW UPCAST ====> ====>");
+		SmartPhone mySmartPhone = (SmartPhone) myPhone;	
+		System.out.println("Brand: " + mySmartPhone.getBrand());
+		mySmartPhone.browseWeb("Page to browse");
+		System.out.println(mySmartPhone.findPosition());
+		mySmartPhone.call("1234");
+		mySmartPhone.printLastNumbers();
+		mySmartPhone.call("001234");
+		mySmartPhone.playGame("wk 11 game");
+		mySmartPhone.ringAlarm("wk 11 alarm");
+		System.out.println();
+	}
+	
+	
 }
-
-
 
 
 
