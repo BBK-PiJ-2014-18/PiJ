50b0c326f4e9954a1ccd536dcf087bb20eac0f72
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-01-17 16:04:11 +0000

    1.3 done

---
+++ day13/x1_practicingTDD/x1_3/Library.java
@@ -0,0 +1,15 @@
+public class Library {
+
+	public String getLibrary() {
+		return "Library name";
+	}
+	
+	public int getID() {
+		return 13;	
+	}
+	
+
+
+
+
+}
---
+++ day13/x1_practicingTDD/x1_3/UserImpl.java
@@ -0,0 +1,36 @@
+public class UserImpl implements User {
+
+	private String name;
+	private int ID;
+	private Library library;
+	
+	public UserImpl (String name) {
+		this.name = name;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}	
+
+	public String getName() {
+		return name;
+	}
+
+	public int getID() {
+		return ID;
+	}	
+
+	public int register(Library library) {
+		this.library = library;
+		this.ID = library.getID();
+		return this.ID;
+	}
+	
+	public Library getLibrary() {
+		return library;
+	}
+	
+
+
+
+}
