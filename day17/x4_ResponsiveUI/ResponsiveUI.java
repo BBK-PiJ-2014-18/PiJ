6e729a759a0e5421457e06c49f1132ce355a4825
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-02-05 19:28:48 +0000

    done, check in class

---
+++ day17/x4_ResponsiveUI/ResponsiveUI.java
@@ -0,0 +1,49 @@
+
+	
+public class ResponsiveUI {	
+
+	private int countDone = 0;
+	
+	public static void main (String[] args) {
+		ResponsiveUI rui = new ResponsiveUI();
+		rui.launch();
+	}
+	
+	public void launch() {
+		DoneList doneList = new DoneList();
+		Task task = null;
+		for (int id = 0; id < 10 ; id++) {
+			int wait = inputWait(id);
+			task = new Task(id, wait, doneList);
+			Thread t = new Thread(task);
+			t.start();
+			printDone(doneList);
+		}
+		//pick up tasks ending after all 10 durations have been entered. 
+		while(countDone < 10) {
+			printDone(doneList);
+		}
+	}
+	
+	public int inputWait(int id) {
+		System.out.print("Enter duration of task (in seconds) " + id + " : ");
+		return 1000 * Integer.parseInt(System.console().readLine());
+	}
+	
+	public void printDone(DoneList doneList) {
+		boolean[] done = doneList.getDoneArray();
+		String result = "";
+		for (int i = 0; i < 10; i++) {
+			if(done[i] == true) {
+				countDone++;
+				result = result + " " + i;
+				done[i] = false;		
+			}	
+		}
+		if(!result.equals("")) {
+			System.out.println("Finished Tasks:" + result);
+		}
+	}
+}
+
+
