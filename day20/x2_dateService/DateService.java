76f847463e998ec11f991b8a65ac4729f65ecb4b
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-03-09 10:01:42 +0000

    done. fine

---
+++ day20/x2_dateService/DateService.java
@@ -0,0 +1,9 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.time.LocalDate;
+
+
+public interface DateService extends Remote{
+
+		public LocalDate getDate() throws RemoteException;
+}
--- day20/x2_dateService/DateServiceB.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.time.LocalDate;
-
-
-public interface DateServiceB extends Remote{
-
-		public String getDate() throws RemoteException;
-}
