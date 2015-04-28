f3a30868dc58433a994ef70ac4790346e39c099a
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-04-28 18:51:43 +0100

    Adding files from day three

---
+++ d3/calc.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class calc
+{
+		private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
+	public static void main(String[] args)
+	{
+double p = 0.0;
+double q = 0.0;
+double answer = 0.0;
+//get inputs
+String str1 = System.console().readLine();
+p = Double.parseDouble(str1);
+System.out.println("Entered: " + p);
+String str2 = System.console().readLine();
+q = Double.parseDouble(str2);
+System.out.println("Entered: " + q);
+System.out.println("What to do?  a = add, s = subtract, m = multiply, d = divide:");
+String str = System.console().readLine();
+System.out.println("Entered: " + str);
+// calculate
+if (str.equals( "a")) {
+	answer = p + q;
+}
+if (str.equals( "s")) {
+	answer = p - q;
+}
+if (str.equals( "m")) {
+	answer = p * q;
+}
+if (str.equals( "d")) {
+	answer = p / q;
+}
+//output
+System.out.println("The answer is: " + answer);
+}}
+
---
+++ d3/calctwo.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class calctwo
+{
+		private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
+	public static void main(String[] args)
+	{
+int answer = 0;
+char find;
+// get function
+String function = "";
+String input = System.console().readLine();
+// find the operator its place in the string
+int length = input.length();
+int i = 0;
+int place = 0;
+while (i < length) {
+	find = input.charAt(i);
+	if (find == '+') {
+		function = "a";
+		place = i;
+	}
+	if (find == '-') {
+		function = "s";
+		place = i;
+	}
+	if (find == '*') {
+			function = "m";
+			place = i;
+	}
+	if (find == '/') {
+			function = "d";
+			place = i;
+	}
+	i++;
+}
+// get the two numbers
+String numOne = input.substring(0,place);
+String numTwo = input.substring(place+1,length);
+int p = Integer.parseInt(numOne);
+int q = Integer.parseInt(numTwo);
+
+// calculate
+if (function.equals( "a")) {
+	answer = p + q;
+}
+if (function.equals( "s")) {
+	answer = p - q;
+}
+if (function.equals( "m")) {
+	answer = p * q;
+}
+if (function.equals( "d")) {
+	answer = p / q;
+}
+//output
+System.out.println("The answer is: " + answer);
+}}
+
