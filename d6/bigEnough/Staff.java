b2112336726e2391f4aaf0f9f13d537acf5b8389
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2015-04-28 19:03:37 +0100

    Adding files from day six

---
+++ d6/bigEnough/Staff.java
@@ -0,0 +1,15 @@
+public class Staff {
+
+	private int[] empIDs;
+	private String[] empNames;
+
+	//constructor
+	private void Staff(int x) {
+		empIDs = new int[x];
+		empNames = new String[x];
+	}
+	//	
+
+	}
+
+}
